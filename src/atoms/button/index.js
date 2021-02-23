bba03177b0c24349e6c76f3aae36df43050a5beb
Anil <anil.s@robosoftin.com>
2021-02-23 19:52:18 +0530

    changes made

--- src/atoms/button/index.js
+++ src/atoms/button/index.js
@@ -1,6 +1,6 @@
 /* @flow */
 import * as React from 'react'
-import classnames from 'classnames'
+// import classnames from 'classnames'
 
 import styles from './style.scss'
 
@@ -21,32 +21,33 @@ export const ButtonSize = {
   LARGE: 'large',
 }
 
-type Props = {
-  type: string,
-  theme: string,
-  size: string,
-  onClick: Function,
-  children: React.Node,
-  className: string,
-  disabled: boolean,
-}
+// type Props = {
+//   type: string,
+//   theme: string,
+//   size: string,
+//   onClick: Function,
+//   children: React.Node,
+//   className: string,
+//   disabled: boolean,
+// }
 
-const Button = (props: Props): React.Element<*> => {
-  const { type, onClick, children, theme, size, className, disabled } = props
-  const classProps: string = classnames(
-    styles.button,
-    styles[theme],
-    styles[size],
-    {
-      [styles.disabled]: disabled,
-    },
-    className
-  )
+const Button = () => {
+  // const { type, onClick, children, theme, size, className, disabled } = Props
+  // const classProps = classnames(
+  //   styles.button,
+  //   styles[theme],
+  //   styles[size],
+  //   {
+  //     [styles.disabled]: disabled,
+  //   },
+  //   className
+  // )
 
   return (
-    <button type={type} onClick={onClick} disabled={disabled} className={classProps}>
-      {children}
-    </button>
+    // <button type={type} onClick={onClick} disabled={disabled} className={classProps}>
+    //   {children}
+    // </button>
+    <div>Anil</div>
   )
 }
 
